67b7188dbcfef2cd87e0651fcd77de757fcfed26
Bryan Cedeño <bryan.cedeo@gmail.com>
2020-10-28 20:00:02 -0500

    variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDataBase;
 if(process.env.NODE_ENV === 'dev'){
 	urlDataBase = 'mongodb://localhost:27017/cafe';
 }else{
-urlDataBase = 'mongodb+srv://complex:R6D9YbvlSwE6Qjri@cluster0.vrxbv.mongodb.net/cafe?retryWrites=true&w=majority'
+urlDataBase = process.env.MONGO_URI;
 
 }
 
